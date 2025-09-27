e8541e5f4fc394ecfd416bf64fc2eb6dc5682a9f
mehal jain <madhurjain2480@gmail.com>
2025-09-27 20:32:57 +0530

    img

--- src/pages/SkincareDetailsAlt.js
+++ src/pages/SkincareDetailsAlt.js
@@ -156,7 +156,7 @@ export default function SkincareDetailsAlt() {
                       }`}
                     >
                       <img
-                        src={img}
+                        src={`https://hs-project-server.onrender.com/uploads/${img}`}
                         alt={`${product.name} ${idx + 1}`}
                         className="w-full h-full object-cover"
                         onError={(e) => {
